a493b512e4abe10212bcdf1f3aed2a72a9654641
Thais Marinho <thais.gomes22@aluno.ce.gov.br>
2026-05-11 16:37:13 -0300

    desenvolvi a tela_rotas + modifiquei menu.php e adcionei o style da tela rotas

--- projeto_transporte/site/menu.php
+++ projeto_transporte/site/menu.php
@@ -6,10 +6,10 @@
 
     <div class="menu-links">
         <a href="main.php"><span class="material-icons">home</span> Home</a>
-        <a href="student.php"><span class="material-icons">person</span> Cadastro</a>
-        <a href="listarstudent.php"><span class="material-icons">list</span> Lista</a>
-        <a href="#"><span class="material-icons">location_on</span> Rotas</a>
-        <a href="logout.php" class="logout"><span class="material-icons">logout</span> Sair</a>
+        <a href="tela.cadastro.php"><span class="material-icons">person</span> Cadastro</a>
+        <a href="lista.alunos.php"><span class="material-icons">list</span> Lista</a>
+        <a href="telarotas.php"><span class="material-icons">location_on</span> Rotas</a>
+        <a href="../index.php" class="logout"><span class="material-icons">logout</span> Sair</a>
     </div>
 
 </nav>
